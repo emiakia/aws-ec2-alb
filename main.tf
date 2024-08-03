01bd223d48d5a7fa9a0f461c77a1aa3b67af292b
Emran Kia <emran.kia@gmail.com>
2024-08-03 22:25:20 +0200

    an ec2 for httpd with security policy inbound 22,80

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ provider "aws" {
 
 
 
-resource "aws_security_group" "scplc_http_ssh" {
-  name        = "scplc_http_ssh"
+resource "aws_security_group" "scgrp_http_ssh" {
+  name        = "scgrp_http_ssh"
   description = "Allow inbound SSH and HTTP, all outbound"
 
   # Allow all outbound traffic
@@ -35,12 +35,12 @@ resource "aws_security_group" "scplc_http_ssh" {
   }
 
   tags = {
-    Name = "scplc_http_ssh"
+    Name = "scgrp_http_ssh"
   }
 }
 
 output "security_group_id" {
-  value = aws_security_group.scplc_http_ssh.id
+  value = aws_security_group.scgrp_http_ssh.id
 }
 
 
@@ -50,10 +50,15 @@ resource "aws_instance" "sample_http_server" {
   key_name      = var.key_name
 
   # Attach the security group created above
-  vpc_security_group_ids = [aws_security_group.scplc_http_ssh.id]
+  vpc_security_group_ids = [aws_security_group.scgrp_http_ssh.id]
 
   tags = {
     Name = "sample_http_server"
+    # Name = var.machine_name
+    # Name       = "${var.machine_name} ${count.index}"
+    # Name       = "Emran ${count.index}"
+    created_by = local.created_by
+    owner      = local.owner
   }
 
   user_data = <<-EOF
@@ -81,58 +86,6 @@ output "ec2_public_ip" {
 }
 
 
-
-# resource "aws_instance" "example" {
-#   ami           = var.ami_id
-#   instance_type = var.instance_type
-#   key_name      = var.key_name
-
-
-#   # User data script
-#   user_data = <<-EOF
-#     #!/bin/bash
-#     # Use this to install your user data (script from top)
-#     # install httpd (Linux 2 version)
-#     yum update -y httpd
-#     service httpd start
-#     chkconfig httpd on
-#     echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-#   EOF
-
-
-#   tags = {
-#     Name = var.machine_name
-#     # Name       = "${var.machine_name} ${count.index}"
-#     # Name       = "Emran ${count.index}"
-#     created_by = local.created_by
-#     owner      = local.owner
-#   }
-# }
-
-
-
-
-
-# resource "aws_iam_user" "Emran01" {
-#   name = "Emran01"
-#   path = "/system/"
-
-#   tags = {
-#     created_by = "Terraform"
-#   }
-# }
-
-# resource "aws_iam_policy" "example_policy" {
-#   name        = "example_policy"
-#   description = "A policy to grant read-only access to S3"
-#   policy      = file("iam_policy.json")
-
-# }
-# resource "aws_iam_policy_attachment" "example_user_policy_attachment" {
-#   name       = "example_user_policy_attachment"
-#   users      = [aws_iam_user.Emran01.name]
-#   policy_arn = aws_iam_policy.example_policy.arn
-# }
 
 # resource "aws_instance" "Emran01" {
 #   ami           = var.ami_id
